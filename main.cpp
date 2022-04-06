c1370acf73e5936a3ac87317d46a97df4fd3dbb9
william-vilcapaza <vilcapazachoquew@gmail.com>
2022-04-06 09:00:27 -0500

    agregar

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, suma;
+    cout << "Ingresar la cantidad de numeros a sumar" << endl;
+    cin>>n;//guardamos en n la cantidad de nueros a sumar
+    if(n%2==0){
+        for(i=0;i<n/2;i++){
+            cout<<i<<endl;
+            suma=i+
+
+        }
+    }
+    else{
+
+    }
+    suma=(n*(n+1))/2;
+    cout<<"La suma es: "<<suma<<endl;
+    return 0;
+}
